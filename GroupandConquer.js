3c18878448f72b36bc31d79c6056f1405f3458b6
Viridiana <vdlre1994@gmail.com>
2019-07-05 12:34:48 -0500

    group and conquer

---
+++ GroupandConquer.js
@@ -0,0 +1,13 @@
+let arregloA =[1224,22,1343,35,152,199,1342,88];
+console.log("La lista 1 es:", arregloA);
+
+let arregloB = [1223,22,1343,35, 1523,199,1342,100];
+console.log("La lista 2 es:", arregloB);
+
+let arregloC = [];
+
+let arregloSinRepetidos=arregloA.filter((valor, indiceActual, arreglo) => arreglo.indexOf(valor) === indiceActual).push.arregloC;
+
+
+
+console.log("Sin repetidos es:", arregloC);
