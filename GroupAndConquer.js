7f598c1cfaf547973a99b5ec1171db03ae7d077f
Alejandra.Moreno <ale.mocr@gmail.com>
2019-07-05 12:23:23 -0500

    ejercicios no resueltos

---
+++ GroupAndConquer.js
@@ -0,0 +1,27 @@
+const items = {
+    name : "list1",
+    data : [1224.22, 1343.35, 152.199, 1342.88]
+};
+
+const items2={
+    name : "list2",
+    data : [1223.22, 1343.35, 1523.199, 1342.100]
+}
+
+var output = {
+    name : "output",
+    data : []
+};
+
+for (let i = 0; i < items.length; i++) {
+    for (let j = 0; j < items2.length; j++) {
+        if (data.items !==data.items2) {
+            data.output.push(data.items)
+            data.output.push(data.items2)
+        }
+        else{
+            data.output.push(data.items)
+        }
+    }
+    
+}
