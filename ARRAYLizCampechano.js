3d29950d359056fbe2b717792bfc3fdc9ae550af
lizcampe <lizcampe@gmail.com>
2019-07-05 12:35:14 -0500

    LizetteArchivos

---
+++ ARRAYLizCampechano.js
@@ -0,0 +1,14 @@
+let a= (1224.22,1343.35, 152.199,1342.88);
+let b= (1223.22,1343.359,1523.199,1342.100);
+
+function c (array){
+	concat.array (a[(n.i)]+b[(n.i)])
+if ((n.i)!==(n.i) 
+return { (n.i)+(0.i)}
+)};
+console.log(c(array);
+
+
+
+
+
